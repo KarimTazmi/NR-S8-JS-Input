09d113a76d97bef7deeafd48e57831539b728ea4
KarimTazmi <57253656+KarimTazmi@users.noreply.github.com>
2020-02-24 19:59:33 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,35 @@
-// function makeGreeting() {
-//   function getName() {
-//     const name = document.querySelector("input").value;
-//     return name;
-//   }
+function makeGreeting() {
+  function getName() {
+    const name = document.querySelector("input").value;
+    return name;
+  }
 
-//   function handleName() {
-//     const inputHandle = getName();
-//     console.log(inputHandle);
+  function handleName() {
+    const inputHandle = getName();
+    console.log(inputHandle);
 
-//     const date = new Date();
-//     const time = date.getHours();
+    const date = new Date();
+    const time = date.getHours();
 
-//     if (time >= 6 && time < 12) {
-//       document.querySelector(".heading__input").innerHTML =
-//         "Goedemorgen, " + inputHandle;
-//     } else if (time >= 12 && time < 17) {
-//       document.querySelector(".heading__input").innerHTML =
-//         "Goedemiddag, " + inputHandle;
-//     } else if (time >= 17 && time < 24) {
-//       document.querySelector(".heading__input").innerHTML =
-//         "Goedenavond, " + inputHandle;
-//     } else if (time >= 24 && time < 6) {
-//       document.querySelector(".header__input").innerHTML =
-//         "Wat laat zeg! Ga eens slapen " + inputHandle;
-//     }
-//   }
+    if (time >= 6 && time < 12) {
+      document.querySelector(".heading__input").innerHTML =
+        "Goedemorgen, " + inputHandle;
+    } else if (time >= 12 && time < 17) {
+      document.querySelector(".heading__input").innerHTML =
+        "Goedemiddag, " + inputHandle;
+    } else if (time >= 17 && time < 24) {
+      document.querySelector(".heading__input").innerHTML =
+        "Goedenavond, " + inputHandle;
+    } else if (time >= 24 && time < 6) {
+      document.querySelector(".header__input").innerHTML =
+        "Wat laat zeg! Ga eens slapen " + inputHandle;
+    }
+  }
 
-//   const inputHandle = document.querySelector('[name = "name"]');
+  const inputHandle = document.querySelector('[name = "name"]');
 
-//   inputHandle.addEventListener("blur", handleName);
-// }
+  inputHandle.addEventListener("blur", handleName);
+}
 
 // makeGreeting();
 function handleNameInput() {
